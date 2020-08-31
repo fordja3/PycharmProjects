682cd5adf1642df442d2be7e6e593092e1c7c662
Jason F <fjord1321@gmail.com>
2020-08-31 11:14:56 -0400

    Python projects for CMSC_455
    Initial Commit_CMSC_455

---
+++ Hw_1/main.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '12345'
+bootstrap = Bootstrap(app)
+
+
+class name_form(FlaskForm):
+    name = StringField('Enter your name: ', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404_error.html"), 404
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template("500_error.html"), 500
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = name_form()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+    
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
---
+++ pythonProject/main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+app=Flask(__name__)
+bootstrap = Bootstrap(app)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
+
+
+
